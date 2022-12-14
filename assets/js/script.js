3938ade902c56ebbd27b8542083fb3204b1bf5ec
Oleg.Grabar <oleg.grabar@ringcentral.com>
2022-12-14 22:10:50 +0300

    feature: add weights on the scale

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 const canvasCoefficient = {
     horizontal: window.innerWidth / 2560,
     vertical: window.innerHeight / 1440,
@@ -7,9 +8,6 @@ const canvasCoefficient = {
     }
 }
 
-
-
-
 class Scales {
     static scalePartVerticalOffset = 50;
     static scalePartHorizontalOffset = 20;
@@ -18,14 +16,20 @@ class Scales {
     constructor() {
         this.baseImg = new Image();
         this.baseImg.src = "assets/img/libra-center.png";
-        this.baseImgWidth = this.baseImg.width
-        this.baseImgHeight = this.baseImg.height;
+        this.baseImgWidth = 0;
+        this.baseImgHeight = 0;
+        this.baseImgX = 0;
+        this.baseImgY = 0;
 
         this.leftScaleImg = new Image();
         this.leftScaleImg.src = "assets/img/libra-left.png";
+        this.leftScaleX = 0;
+        this.leftScaleY = 0;
         this.leftWeight = [];
 
         this.rightScaleImg = new Image();
+        this.rightScaleX = 0;
+        this.rightScaleY = 0;
         this.rightScaleImg.src = "assets/img/libra-right.png";
         this.rightWeight = [];
 
@@ -44,33 +48,44 @@ class Scales {
         this.baseImgWidth = this.baseImg.width * canvasCoefficient.horizontal;
         this.baseImgHeight = this.baseImg.height * canvasCoefficient.vertical;
 
-        const sx = ctx.canvas.width / 2 - this.baseImgWidth / 2;
-        const sy = ctx.canvas.height - this.baseImgHeight;
+        this.baseImgX = ctx.canvas.width / 2 - this.baseImgWidth / 2;
+        this.baseImgY = ctx.canvas.height - this.baseImgHeight;
 
-        ctx.drawImage(this.baseImg, sx, ctx.canvas.height - this.baseImgHeight, this.baseImgWidth, this.baseImgHeight);
+        ctx.drawImage(this.baseImg, this.baseImgX, this.baseImgY, this.baseImgWidth, this.baseImgHeight);
     }
 
     drawLeft(ctx) {
         this.scaleImgWidth = this.leftScaleImg.width * canvasCoefficient.horizontal;
         this.scaleImgHeight = this.leftScaleImg.height * canvasCoefficient.vertical;
 
-        const sx = ctx.canvas.width / 2 - this.scaleImgWidth - Scales.scalePartHorizontalOffset * canvasCoefficient.vertical;
-        const sy = ctx.canvas.height - this.baseImgHeight + Scales.scalePartVerticalOffset * canvasCoefficient.vertical
+        this.leftScaleX = ctx.canvas.width / 2 - this.scaleImgWidth - Scales.scalePartHorizontalOffset * canvasCoefficient.vertical;
+        this.leftScaleY = ctx.canvas.height - this.baseImgHeight + Scales.scalePartVerticalOffset * canvasCoefficient.vertical
             + Scales.weightDifferenceCoefficient * this.weightDifference;
 
-        ctx.drawImage(this.leftScaleImg, sx, sy, this.scaleImgWidth, this.scaleImgHeight);
-        console.log()
+        ctx.drawImage(this.leftScaleImg, this.leftScaleX, this.leftScaleY, this.scaleImgWidth, this.scaleImgHeight);
+
+        this.leftWeight.forEach((weight, index) => {
+            weight.x = this.leftScaleX + index * 100 * canvasCoefficient.horizontal;
+            weight.y = this.leftScaleY - weight.height - 10;
+            weight.draw(ctx);
+        });
     }
 
     drawRight(ctx) {
         this.scaleImgWidth = this.rightScaleImg.width * canvasCoefficient.horizontal;
         this.scaleImgHeight = this.rightScaleImg.height * canvasCoefficient.vertical;
 
-        const sx = ctx.canvas.width / 2 + Scales.scalePartHorizontalOffset * canvasCoefficient.horizontal;
-        const sy = ctx.canvas.height - this.baseImgHeight + Scales.scalePartVerticalOffset * canvasCoefficient.vertical
+        this.rightScaleX = ctx.canvas.width / 2 + Scales.scalePartHorizontalOffset * canvasCoefficient.horizontal;
+        this.rightScaleY = ctx.canvas.height - this.baseImgHeight + Scales.scalePartVerticalOffset * canvasCoefficient.vertical
             - Scales.weightDifferenceCoefficient * this.weightDifference;
 
-        ctx.drawImage(this.rightScaleImg, sx, sy, this.scaleImgWidth, this.scaleImgHeight);
+        ctx.drawImage(this.rightScaleImg, this.rightScaleX, this.rightScaleY, this.scaleImgWidth, this.scaleImgHeight);
+
+        this.rightWeight.forEach((weight, index) => {
+           weight.x = this.rightScaleX + index * 100 * canvasCoefficient.horizontal;
+           weight.y = this.rightScaleY - weight.height - 10;
+           weight.draw(ctx);
+        });
     }
 
     updateScales(ctx) {
@@ -96,24 +111,21 @@ class Weight {
         this.height = 0;
 
         this.isSelected = false;
-        this.isOnTable = false;
     }
 
     draw(ctx) {
         this.updateWidthAndHeight();
-
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
         ctx.fillText(this.weight.toString(), this.x + (this.width / 2 - 5), this.y + (this.height - 10));
     }
 
     updateWidthAndHeight() {
-        this.width = canvasCoefficient.horizontal * (this.img.width + this.img.width * this.weight * 0.1) / 3;
-        this.height = canvasCoefficient.vertical * (this.img.height + this.img.height * this.weight * 0.1) / 3;
+        this.width = canvasCoefficient.horizontal * (this.img.width + this.img.width * this.weight * 0.1) / 1.7;
+        this.height = canvasCoefficient.vertical * (this.img.height + this.img.height * this.weight * 0.1) / 1.7;
     }
 }
 
 class Table {
-//TODO weightsOnTable move here
     constructor() {
         this.weightsOnTable = [new Weight(), new Weight(), new Weight()];
     }
@@ -137,9 +149,13 @@ class Table {
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 ctx.font = "20px serif";
+ctx.canvas.width = ctx.canvas.offsetWidth;
+ctx.canvas.height = ctx.canvas.offsetHeight;
 
 let fallingWeight = new Weight();
 const table = new Table();
+const scales = new Scales();
+
 
 const mouse = {
     x: 0,
@@ -147,61 +163,71 @@ const mouse = {
     down: false
 }
 
-window.onmousemove = function(e) {
-    mouse.x = e.pageX;
-    mouse.y = e.pageY;
+
+window.onmousemove = function (e) {
+    mouse.x = e.pageX - scrollX;
+    mouse.y = e.pageY - scrollY;
 };
 
-window.onmousedown = function(e) {
-    mouse.down = true;
-    if (Math.abs((mouse.x - fallingWeight.x)) <= fallingWeight.width * 1.1 && Math.abs((mouse.y - fallingWeight.y)) <= fallingWeight.height * 1.1) {
+function fallingWeightClicked() {
+    const clickOnFallingWeight = mouse.x >= fallingWeight.x && mouse.x <= (fallingWeight.x + fallingWeight.width) && mouse.y >= fallingWeight.y && mouse.y <= (fallingWeight.y + fallingWeight.height);
+    if (clickOnFallingWeight) {
         fallingWeight.isSelected = true;
     }
+}
+
+window.onmousedown = function (e) {
+    mouse.down = true;
+    fallingWeightClicked();
 };
 
+window.onmouseup = function (e) {
+    mouse.down = false;
+    fallingWeight.x = mouse.x;
+    fallingWeight.y = mouse.y;
+
+    if (fallingWeight.x >= scales.leftScaleX * 0.9 && fallingWeight.x <= scales.leftScaleX + scales.scaleImgWidth &&
+        fallingWeight.y <= scales.baseImgY * 1.5 && fallingWeight.y >= scales.baseImgY * 0.5) {
+        scales.leftWeight.push(fallingWeight);
+    }
+
+    if (fallingWeight.x >= scales.rightScaleX * 0.9 && fallingWeight.x <= scales.rightScaleX + scales.scaleImgWidth &&
+        fallingWeight.y <= scales.baseImgY * 1.5 && fallingWeight.y >= scales.baseImgY * 0.5) {
+        scales.rightWeight.push(fallingWeight);
+    }
+
+    fallingWeight = new Weight();
+}
+
+
+function game() {
+    canvasCoefficient.update(ctx);
+    scales.updateScales(ctx);
+
+    if (scales.weightDifference === 0 && scales.leftWeight.length > 0 && scales.rightWeight.length > 0) {
+        //TODO win
+    }
+
+    if (fallingWeight.isSelected) {
+        fallingWeight.x = mouse.x - fallingWeight.width / 2;
+        fallingWeight.y = mouse.y - fallingWeight.height / 2;
+    } else {
+        fallingWeight.y = fallingWeight.y + 1;
+    }
+
+    fallingWeight.draw(ctx);
+    table.draw(ctx)
+
+
+    //TODO change to the Table class method
+    //weightsOnTable.forEach((weightOnTable) => weightOnTable.draw(ctx));
+    requestAnimationFrame(game);
+}
 
 function start() {
-    const scales = new Scales();
     console.log(ctx)
     console.log(scales);
     scales.updateScales(ctx);
 
-    setTimeout(() => {}, 2000)
-
-    setInterval(function () {
-        ctx.canvas.width = window.innerWidth;
-        ctx.canvas.height = window.innerHeight;
-        canvasCoefficient.update(ctx);
-        scales.updateScales(ctx);
-        if (scales.weightDifference === 0 && scales.leftWeight.length > 0 && scales.rightWeight.length > 0) {
-            //TODO win
-        }
-
-        if (!fallingWeight.selected) {
-            fallingWeight.y = fallingWeight.y + 1;
-        } else {
-            fallingWeight.x = mouse.x;
-            fallingWeight.y = mouse.y;
-        }
-
-        fallingWeight.draw(ctx);
-        if (fallingWeight.y >= canvas.height) {
-            fallingWeight = new Weight()
-        }
-        table.draw(ctx)
-
-
-        //TODO change to the Table class method
-        //weightsOnTable.forEach((weightOnTable) => weightOnTable.draw(ctx));
-    }, 100);
+    game();
 }
-
-function isGameOver() {
-
-}
-
-function isWin() {
-
-}
-
-
