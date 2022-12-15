b36e9619286fa39d2c7a22df62dbc338ab24c113
Oleg.Grabar <oleg.grabar@ringcentral.com>
2022-12-15 15:48:52 +0300

    fixes: fix fallingWeight update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-
 const canvasCoefficient = {
-    horizontal: window.innerWidth / 2560,
-    vertical: window.innerHeight / 1440,
+    horizontal: 1,
+    vertical: 1,
     update(ctx) {
-        this.horizontal = ctx.canvas.width / 2560;
-        this.vertical = ctx.canvas.height / 1440
+        this.horizontal = window.innerWidth / 2560;
+        this.vertical = window.innerHeight / 1440 ;
     }
 }
 
@@ -36,6 +35,8 @@ class Scales {
         this.scaleImgWidth = this.leftScaleImg.width;
         this.scaleImgHeight = this.leftScaleImg.height;
 
+        this.weightOffset = 100;
+
         this.weightDifference = 0;
     }
 
@@ -65,7 +66,7 @@ class Scales {
         ctx.drawImage(this.leftScaleImg, this.leftScaleX, this.leftScaleY, this.scaleImgWidth, this.scaleImgHeight);
 
         this.leftWeight.forEach((weight, index) => {
-            weight.x = this.leftScaleX + index * 100 * canvasCoefficient.horizontal;
+            weight.x = this.leftScaleX + index * this.weightOffset * canvasCoefficient.horizontal;
             weight.y = this.leftScaleY - weight.height - 10;
             weight.draw(ctx);
         });
@@ -82,14 +83,13 @@ class Scales {
         ctx.drawImage(this.rightScaleImg, this.rightScaleX, this.rightScaleY, this.scaleImgWidth, this.scaleImgHeight);
 
         this.rightWeight.forEach((weight, index) => {
-           weight.x = this.rightScaleX + index * 100 * canvasCoefficient.horizontal;
-           weight.y = this.rightScaleY - weight.height - 10;
-           weight.draw(ctx);
+            weight.x = this.rightScaleX + index * this.weightOffset * canvasCoefficient.horizontal;
+            weight.y = this.rightScaleY - weight.height - 10;
+            weight.draw(ctx);
         });
     }
 
     updateScales(ctx) {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         this.calculateWeightDifference();
         this.drawBase(ctx);
         this.drawLeft(ctx);
@@ -104,7 +104,7 @@ class Weight {
         this.img = new Image();
         this.img.src = "assets/img/bob.png";
 
-        this.x = Math.floor(Math.random() * 500) * canvasCoefficient.horizontal;
+        this.x = this.getRandomX();
         this.y = 0;
 
         this.width = 0;
@@ -120,41 +120,87 @@ class Weight {
     }
 
     updateWidthAndHeight() {
-        this.width = canvasCoefficient.horizontal * (this.img.width + this.img.width * this.weight * 0.1) / 1.7;
-        this.height = canvasCoefficient.vertical * (this.img.height + this.img.height * this.weight * 0.1) / 1.7;
+        this.width = canvasCoefficient.horizontal * (this.img.width + this.img.width * this.weight * 0.1) / 2;
+        this.height = canvasCoefficient.vertical * (this.img.height + this.img.height * this.weight * 0.1) / 2;
+    }
+
+    getRandomX() {
+        return Math.floor(Math.random() * 500 * canvasCoefficient.horizontal);
     }
 }
 
 class Table {
     constructor() {
-        this.weightsOnTable = [new Weight(), new Weight(), new Weight()];
+        this.weightsOnTable = [];
+        this.maxWeightsOnTableSize = 3;
+        this.maxWeightHeight = 0;
+
+        this.x = 0;
+        this.y = 0;
+        this.width = 0;
+        this.lineWitdth = 10;
+        this.color = '#e4b04a';
     }
 
     draw(ctx) {
+        this.x = ctx.canvas.width * 0.7
+        this.width = ctx.canvas.width * (0.05 * this.maxWeightsOnTableSize);
+        this.y = ctx.canvas.height - 300 * canvasCoefficient.vertical;
+
         ctx.beginPath();
-        ctx.moveTo(ctx.canvas.width * 0.7, ctx.canvas.height - 300);
-        ctx.lineTo(ctx.canvas.width * 0.8, ctx.canvas.height - 300);
-        ctx.lineWidth = 10;
-        ctx.strokeStyle = '#e4b04a';
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x + this.width, this.y);
+        ctx.lineWidth = this.lineWitdth;
+        ctx.strokeStyle = this.color;
         ctx.stroke();
+
         this.weightsOnTable.forEach((weight, index) => {
-            weight.x = ctx.canvas.width * 0.7 + index * 100 * canvasCoefficient.horizontal;
-            weight.y = ctx.canvas.height - 300 - weight.height;
+            weight.x = this.x + this.getWeightOffset(index);
+            weight.y = this.y - weight.height;
             weight.draw(ctx)
         });
     }
 
+    getWeightOffset(index) {
+        return index * this.width / this.maxWeightsOnTableSize;
+    }
+
+    addWeight(weight) {
+        if (weight.height > this.maxWeightHeight) {
+            this.maxWeightHeight = weight.height
+        }
+
+        if (this.weightsOnTable.length < this.maxWeightsOnTableSize) {
+            this.weightsOnTable.push(weight)
+        } else {
+            let weightIndex;
+            for (let i = 0; i < this.weightsOnTable.length; i++) {
+                if (weight.x >= this.x + this.getWeightOffset(i) * 0.85 && weight.x <= this.x + this.getWeightOffset(i + 1) * 1.15) {
+                    weightIndex = i;
+                }
+            }
+            this.weightsOnTable.splice(weightIndex, 1, weight);
+        }
+    }
+
+    removeWeight(index) {
+        this.weightsOnTable.splice(index, 1);
+    }
 }
 
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 ctx.font = "20px serif";
-ctx.canvas.width = ctx.canvas.offsetWidth;
-ctx.canvas.height = ctx.canvas.offsetHeight;
+ctx.canvas.width = 2000;
+ctx.canvas.height = 1440;
 
 let fallingWeight = new Weight();
+let weightFromTable = new Weight();
 const table = new Table();
 const scales = new Scales();
+
+let win = false;
+let loose = false;
 
 
 const mouse = {
@@ -169,43 +215,101 @@ window.onmousemove = function (e) {
     mouse.y = e.pageY - scrollY;
 };
 
-function fallingWeightClicked() {
-    const clickOnFallingWeight = mouse.x >= fallingWeight.x && mouse.x <= (fallingWeight.x + fallingWeight.width) && mouse.y >= fallingWeight.y && mouse.y <= (fallingWeight.y + fallingWeight.height);
-    if (clickOnFallingWeight) {
-        fallingWeight.isSelected = true;
-    }
-}
 
 window.onmousedown = function (e) {
     mouse.down = true;
-    fallingWeightClicked();
+
+    //click on the falling weight
+    if (mouse.x >= fallingWeight.x && mouse.x <= (fallingWeight.x + fallingWeight.width) && mouse.y >= fallingWeight.y && mouse.y <= (fallingWeight.y + fallingWeight.height)) {
+        fallingWeight.isSelected = true;
+    }
+
+    //click on the table
+    if (mouse.x >= table.x && mouse.x <= table.x + table.width && mouse.y <= table.y && mouse.y >= table.y - table.maxWeightHeight) {
+        let weightIndex;
+        for (let i = 0; i < table.weightsOnTable.length; i++) {
+            if (mouse.x >= table.x + table.getWeightOffset(i) * 0.85 && mouse.x <= table.x + table.getWeightOffset(i + 1) * 1.15) {
+                weightIndex = i;
+            }
+        }
+        weightFromTable = table.weightsOnTable[weightIndex];
+        table.weightsOnTable.splice(weightIndex, 1);
+        weightFromTable.isSelected = true;
+    }
 };
 
 window.onmouseup = function (e) {
     mouse.down = false;
-    fallingWeight.x = mouse.x;
-    fallingWeight.y = mouse.y;
 
-    if (fallingWeight.x >= scales.leftScaleX * 0.9 && fallingWeight.x <= scales.leftScaleX + scales.scaleImgWidth &&
-        fallingWeight.y <= scales.baseImgY * 1.5 && fallingWeight.y >= scales.baseImgY * 0.5) {
-        scales.leftWeight.push(fallingWeight);
+    if (fallingWeight.isSelected) {
+        if (fallingWeight.x >= scales.leftScaleX * 0.9 && fallingWeight.x <= scales.leftScaleX + scales.scaleImgWidth &&
+            fallingWeight.y <= scales.baseImgY * 1.5 && fallingWeight.y >= scales.baseImgY * 0.5) {
+            scales.leftWeight.push(fallingWeight);
+            fallingWeight = new Weight();
+            return;
+        }
+
+        if (fallingWeight.x >= scales.rightScaleX * 0.9 && fallingWeight.x <= scales.rightScaleX + scales.scaleImgWidth &&
+            fallingWeight.y <= scales.baseImgY * 1.5 && fallingWeight.y >= scales.baseImgY * 0.5) {
+            scales.rightWeight.push(fallingWeight);
+            fallingWeight = new Weight();
+            return;
+        }
+
+        if (fallingWeight.x >= table.x * 0.9 && fallingWeight.x <= (table.x + table.width) * 1.1 &&
+            fallingWeight.y <= table.y * 1.5 && fallingWeight.y >= table.y * 0.5) {
+            table.addWeight(fallingWeight);
+            fallingWeight = new Weight();
+            return;
+        }
+
+        fallingWeight.x = fallingWeight.getRandomX();
+        fallingWeight.isSelected = false;
+
+        return;
     }
 
-    if (fallingWeight.x >= scales.rightScaleX * 0.9 && fallingWeight.x <= scales.rightScaleX + scales.scaleImgWidth &&
-        fallingWeight.y <= scales.baseImgY * 1.5 && fallingWeight.y >= scales.baseImgY * 0.5) {
-        scales.rightWeight.push(fallingWeight);
+    if (weightFromTable.isSelected) {
+        if (weightFromTable.x >= scales.leftScaleX * 0.9 && weightFromTable.x <= scales.leftScaleX + scales.scaleImgWidth &&
+            weightFromTable.y <= scales.baseImgY * 1.5 && weightFromTable.y >= scales.baseImgY * 0.5) {
+            scales.leftWeight.push(weightFromTable);
+            weightFromTable = new Weight();
+            return;
+        }
+
+        if (weightFromTable.x >= scales.rightScaleX * 0.9 && weightFromTable.x <= scales.rightScaleX + scales.scaleImgWidth &&
+            weightFromTable.y <= scales.baseImgY * 1.5 && weightFromTable.y >= scales.baseImgY * 0.5) {
+            scales.rightWeight.push(weightFromTable);
+            weightFromTable = new Weight();
+            return;
+        }
+
+        if (weightFromTable.x >= table.x && weightFromTable.x <= (table.x + table.width)  &&
+            weightFromTable.y <= table.y * 1.5 && weightFromTable.y >= table.y * 0.5) {
+            table.addWeight(weightFromTable);
+            weightFromTable = new Weight();
+            return;
+        }
+
+        if (weightFromTable.x <= scales.leftScaleX || weightFromTable.x > table.x + table.width * 1.1) {
+            weightFromTable = new Weight();
+            return;
+        }
     }
 
-    fallingWeight = new Weight();
 }
 
 
 function game() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     canvasCoefficient.update(ctx);
+    ctx.canvas.width = 2000 * canvasCoefficient.horizontal;
+    ctx.canvas.height = 1440 * canvasCoefficient.vertical;
+    console.log(canvas.clientWidth);
     scales.updateScales(ctx);
 
     if (scales.weightDifference === 0 && scales.leftWeight.length > 0 && scales.rightWeight.length > 0) {
-        //TODO win
+        alert("win")
     }
 
     if (fallingWeight.isSelected) {
@@ -214,20 +318,24 @@ function game() {
     } else {
         fallingWeight.y = fallingWeight.y + 1;
     }
-
     fallingWeight.draw(ctx);
+
+    if (weightFromTable.isSelected) {
+        weightFromTable.x = mouse.x - weightFromTable.width / 2;
+        weightFromTable.y = mouse.y - weightFromTable.height / 2;
+        weightFromTable.draw(ctx);
+    }
+
+    if (fallingWeight.y >= ctx.canvas.height - fallingWeight.height / 2) {
+        fallingWeight = new Weight();
+    }
+
     table.draw(ctx)
 
-
-    //TODO change to the Table class method
-    //weightsOnTable.forEach((weightOnTable) => weightOnTable.draw(ctx));
-    requestAnimationFrame(game);
 }
 
 function start() {
-    console.log(ctx)
-    console.log(scales);
-    scales.updateScales(ctx);
-
-    game();
+    setInterval(
+        () => {game()}, 5
+    )
 }
